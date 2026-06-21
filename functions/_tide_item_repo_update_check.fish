426d1232deb748e863a10980e624f01a09f936d5
Leo <lcm@keemail.me>
2026-06-21 19:16:51 +0100

    Add grey colour and massively improve repo_update_check thing

--- functions/_tide_item_repo_update_check.fish
+++ functions/_tide_item_repo_update_check.fish
@@ -4,32 +4,36 @@ function _tide_item_repo_update_check
     else
         echo "repo update check configuration$BR not found$RESET, add repos to$BY ~/.config/repos.conf"
     end
-    for repo in $repos
+    for i in (seq (count $repos))
         if ! test -f /tmp/last_fetch
             echo (date +%H) > /tmp/last_fetch
         end
         if test (cat /tmp/last_fetch) != (date +%H)
-            git -C "$repo" fetch >/dev/null 2>&1 &
+            git -C "$repos[$i]" fetch >/dev/null 2>&1 &
             echo (date +%H) > /tmp/last_fetch
         end
-        set branch (git -C "$repo" symbolic-ref --quiet --short HEAD)
-        set behind (git -C "$repo" rev-list --count HEAD..origin/"$branch")
-        set ahead (git -C "$repo" rev-list --count origin/"$branch"..HEAD)
-        set unstaged (git -C "$repo" status --porcelain | wc -l)
-        set name (basename (git -C "$repo" remote get-url origin))
-        set msg "repo$BY $name$RESET:"
+        set branch (git -C "$repos[$i]" symbolic-ref --quiet --short HEAD)
+        set remote (git -C "$repos[$i]" remote)
+        set behind (git -C "$repos[$i]" rev-list --count HEAD.."$remote"/"$branch")
+        set ahead (git -C "$repos[$i]" rev-list --count "$remote"/"$branch"..HEAD)
+        set unstaged (git -C "$repos[$i]" status --porcelain | wc -l)
+        set name (basename (git -C "$repos[$i]" remote get-url "$remote"))
+        set msg "$BY $name$RESET:"
         if test $behind = 0 -a $ahead = 0 -a $unstaged = 0
             continue
         end
         if test $behind -gt 0
-            set msg "$BW$msg$BY $behind$RESET$BR behind$RESET"
+            set msg "$BW$msg$BW $behind$RESET$BR b$RESET"
         end
         if test $ahead -gt 0
-            set msg "$BW$msg$BY $ahead$RESET$BG ahead$RESET"
+            set msg "$BW$msg$BW $ahead$RESET$BG a$RESET"
         end
         if test $unstaged -gt 0
-            set msg "$BW$msg$BY $unstaged$BC unstaged$RESET"
+            set msg "$BW$msg$BW $unstaged$BC u$RESET"
         end
-        echo -n " $msg"
+        echo -n "$msg"
+        if test "$i" != (count $repos)
+            echo -n "$BGR ──"
+        end
     end
 end
