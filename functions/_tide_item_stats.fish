c2190161f52bb2ea5314d264254dddbe64c261c4
merll002 <lcm@keemail.me>
2026-07-01 15:48:44 +0100

    add color change for cpu

--- functions/_tide_item_stats.fish
+++ functions/_tide_item_stats.fish
@@ -1,5 +1,7 @@
 function _tide_item_stats
     echo -n $BW"  $BY$(history |wc -l)"
     echo -n $BW"  $BC$(uptime -p)"
-    echo -n $BW"  $BR$(load_avg=(uptime -r|cut -d' ' -f4) math "$load_avg / $(nproc) * 100")%"
+    set load (load_avg=(uptime -r|cut -d' ' -f4) math "$load_avg / $(nproc) * 100")
+    set colour "$(/opt/scripts/num_to_ansi "$load")"
+    echo -en "$BW  $colour$load%"
 end
