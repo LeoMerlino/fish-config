f7c477b16cf14a32390d3c3b194f40fb9e7adea2
Leo <lcm@keemail.me>
2026-08-29 16:08:38 +0100

    If uptime -r is not available don't freak out

--- functions/_tide_item_stats.fish
+++ functions/_tide_item_stats.fish
@@ -2,7 +2,10 @@ function _tide_item_stats
     echo -n $BW"  $BY$(history |wc -l)"
     uptime | awk '{print $3}' | tr ':' '\n' | tr -d , | read -L h m
     echo -n $BW  $BC"up "{$h}h {$m}m
-    set load (load_avg=(uptime -r|cut -d' ' -f4) math "$load_avg / $(nproc) * 100")
-    set colour "$(/opt/scripts/num_to_ansi "$load")"
-    echo -en "$BW  $colour$load%"
+    uptime -r >/dev/null 2>&1 && {
+        set load_avg (uptime -r|cut -d' ' -f4)
+        set load (math "$load_avg / $(nproc) * 100")
+        set colour "$(/opt/scripts/num_to_ansi "$load")"
+        echo -en "$BW  $colour$load%"
+    }
 end
