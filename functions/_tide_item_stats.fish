b55b0c9b46a2b42ce08cfd6a3000b5f0f91b6d1f
Leo <lcm@keemail.me>
2026-08-29 16:20:53 +0100

    maybe this fixes?

--- functions/_tide_item_stats.fish
+++ functions/_tide_item_stats.fish
@@ -3,7 +3,7 @@ function _tide_item_stats
     uptime | awk '{print $3}' | tr ':' '\n' | tr -d , | read -L h m
     echo -n $BW  $BC"up "{$h}h {$m}m
     uptime -r >/dev/null 2>&1 && {
-        set load_avg (uptime -r|cut -d' ' -f4)
+        set load_avg (string split ' ' -- (uptime -r))[4]
         set load (math "$load_avg / $(nproc) * 100")
         set colour "$(/opt/scripts/num_to_ansi "$load")"
         echo -en "$BW  $colour$load%"
